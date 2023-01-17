981ace5f25a9a300941470a2089375248e91e133
Sunset-kim <wood2002@naver.com>
2023-01-17 20:22:03 +0900

    build: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
+const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
+
 function range(start, end) {
   const mylength = end - start + 1;
   return Array.from({ length: mylength }, (_, i) => start + i);
@@ -11,8 +15,13 @@ module.exports = {
   theme: {
     darkMode: "class",
     extend: {
+      borderWidth: px0_10,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      minWidth: px0_200,
+      minHeight: px0_200,
       spacing: {
-        ...range(1, 100).reduce((acc, px) => {
+        ...range(1, 200).reduce((acc, px) => {
           acc[`${px}pxr`] = pxToRem(px);
           return acc;
         }, {}),
