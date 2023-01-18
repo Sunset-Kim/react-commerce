a3cbe510b67e753c6f89c3475d39db7cc13b61d5
Sunset-kim <wood2002@naver.com>
2023-01-19 01:23:45 +0900

    fix: button component props

--- src/features/ui/Button/button.tsx
+++ src/features/ui/Button/button.tsx
@@ -11,17 +11,23 @@ export interface ButtonProps extends UIProps<HTMLButtonElement> {
 
 type Variants = "primary" | "outlined";
 
-const buttonStyle = tw`px-4 py-2 rounded w-full flex justify-center items-center`;
+const buttonStyle = tw`px-4 py-2 rounded w-full flex justify-center items-center outline-0 hover:brightness-110 focus:ring-2 focus:ring-offset-2 transition-colors`;
 
 const variantsStyle: { [variants in Variants]: TwStyle } = {
   primary: tw`text-white`,
-  outlined: tw`border text-slate-700`,
+  outlined: tw`border-2 bg-transparent`,
 };
 
 const colorStyle: { [color in Color]: TwStyle } = {
-  sky: tw`bg-sky-400 border-sky-400`,
-  green: tw`bg-emerald-400 border-emerald-400 `,
-  red: tw`bg-red-400 border-red-400`,
+  sky: tw`bg-sky-400 ring-sky-300`,
+  green: tw`bg-emerald-400 ring-emerald-300`,
+  red: tw`bg-red-400 ring-red-300`,
+};
+
+const outlineColorStyle: { [color in Color]: TwStyle } = {
+  red: tw`border-red-400 ring-red-300 hover:bg-red-400 hover:text-white`,
+  sky: tw`border-sky-400 ring-sky-300 hover:bg-sky-400 hover:text-white`,
+  green: tw`border-green-400 ring-green-300 hover:bg-green-400 hover:text-white`,
 };
 
 export default function Button({
@@ -35,8 +41,9 @@ export default function Button({
     <button
       css={[
         buttonStyle,
+        variants === "primary" && colorStyle[color],
+        variants === "outlined" && outlineColorStyle[color],
         variantsStyle[variants],
-        color && colorStyle[color],
         sx,
       ]}
       {...props}
