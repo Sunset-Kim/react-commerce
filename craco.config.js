943873890a7a33784cf4c87cb5d9f4497f5de49c
Sunset-kim <wood2002@naver.com>
2023-01-25 16:09:39 +0900

    build: jest config

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,23 @@
 const path = require("path");
+const TEST_REGEX = "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$";
 
 module.exports = {
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  jest: {
+    verbose: true,
+    testRegex: TEST_REGEX,
+    moduleNameMapper: {
+      "^@/(.*)$": "<rootDir>/src/$1",
+    },
+    testPathIgnorePatterns: [
+      "<rootDir>/build/",
+      "<rootDir>/node_modules/",
+      "<rootDir>/dist/",
+    ],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
   },
 };
