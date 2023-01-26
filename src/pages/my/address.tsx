78037a7e811714e36dcd92c3ad804d9d14dc9506
Sunset-kim <wood2002@naver.com>
2023-01-26 23:01:01 +0900

    fix: address가 정상적으로 응답을 받았지만 없을때

--- src/pages/my/address.tsx
+++ src/pages/my/address.tsx
@@ -13,28 +13,33 @@ export default function Adress() {
     return <>로딩중</>;
   }
 
-  if (!data) {
+  if (!data || data.length === 0) {
     return (
-      <div className="flex flex-col items-center">
-        <Text size="sm">배송지 정보가 없습니다.</Text>
-        <Text size="sm">새 배송지를 등록해주세요</Text>
-        <Button
-          variants="outlined"
-          onClick={() => setToggle()}
-          size="md"
-          sx={tw`mt-4 rounded-xl border-gray-300 text-xs`}
-        >
-          새 배송지 추가
-        </Button>
-      </div>
+      <>
+        <div className="flex flex-col items-center">
+          <Text size="sm">배송지 정보가 없습니다.</Text>
+          <Text size="sm">새 배송지를 등록해주세요</Text>
+          <Button
+            variants="outlined"
+            onClick={() => setToggle()}
+            size="md"
+            sx={tw`mt-4 rounded-xl border-gray-300 text-xs`}
+          >
+            새 배송지 추가
+          </Button>
+        </div>
+        <div>
+          {isOpen && (
+            <AddressForm onSubmit={(address) => addAddress(address)} />
+          )}
+        </div>
+      </>
     );
   }
 
   return (
     <>
       <div>{data && <AddressList list={data} />}</div>
-
-      {isOpen && <AddressForm onSubmit={(address) => addAddress(address)} />}
     </>
   );
 }
